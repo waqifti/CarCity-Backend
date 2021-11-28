770a0493b0184ba5edc9ce7893b2f96629b5b7b0
syediftekharahmed <waqifti@hotmail.com>
2021-11-28 23:30:49 +0500

    Jobs Assignment 5

--- src/main/java/com/carcity/CarCity/Backend/dataentities/Jobs.java
+++ src/main/java/com/carcity/CarCity/Backend/dataentities/Jobs.java
@@ -6,18 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -59,7 +48,7 @@ public class Jobs {
 	@OneToOne
 	private ApplicationUser createdby;
 
-	@OneToOne
+	@OneToOne(fetch = FetchType.EAGER)
 	private ApplicationUser assignedto;
 
 	@OneToOne
